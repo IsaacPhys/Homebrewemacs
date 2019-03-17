16a1e21028a7f60cec36955ab096bd9787ed4e81
IsaacPhys <hepisaacf@gmail.com>
2019-03-16 23:59:29 -0400

    font and logo

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+   '("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -11,7 +11,7 @@
 (setq-default cursor-type 'bar)
 
 ;; Full Screen
-					;(toggle-frame-fullscreen)
+;(toggle-frame-fullscreen)
 
 ;(setq default-frame-alist
 ;      `((top . 0)
@@ -22,12 +22,27 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+;; ----------------------------------------------
+;; Set Spacemacs Logo
+;; ----------------------------------------------
+(defcustom MySpacemacs-logo (expand-file-name "spacemacs.png" user-emacs-directory)
+  "Set Centaur logo. nil means official logo."
+  :type 'string)
+(if (display-graphic-p)
+     (progn    
+    (setq dashboard-startup-banner (or MySpacemacs-logo 'official))
+    (setq fancy-splash-image MySpacemacs-logo)))
 ;; -----------------------------------------------
 ;; Font Set
 ;; -----------------------------------------------
 (add-to-list 'default-frame-alist
              '(font . "Source Code Pro-14"))
 (set-frame-font "Source Code Pro-14")
+;;中文字体配置
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                    (font-spec :family "STKaiti" :size 16)))
 
 ;;-----------------------------------------------
 ;; Modeline
@@ -48,7 +63,7 @@
 (if (display-graphic-p)
    ; (progn
     ;  (require 'doom-themes)
-    (load-theme 'doom-one t)
+    (load-theme 'spacemacs-dark t)
   (load-theme 'homebrew t))
 
 
