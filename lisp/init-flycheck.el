3f43d5828e047cfed8530c96435f1304530bac6c
IsaacPhys <hepisaacf@gmail.com>
2019-03-16 21:37:56 -0400

    a

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,7 @@
   :diminish flycheck-mode
   :hook (after-init . global-flycheck-mode)
   :config
- ; (setq flycheck-indication-mode 'right-fringe)
+  (setq flycheck-indication-mode 'left-fringe)
   (setq flycheck-emacs-lisp-load-path 'inherit)
   (setq flycheck-checker-error-threshold 10000)
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
